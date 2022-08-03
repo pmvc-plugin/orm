4a752280e17cfb817e06f85ef8e970939942dd35
Hill <hill@kimo.com>
2022-08-03 11:15:45 +0800

    backup

--- src/WhereTrait.php
+++ src/WhereTrait.php
@@ -4,71 +4,93 @@ namespace PMVC\PlugIn\orm;
 
 trait WhereTrait
 {
-    public function exact()
+    private $_where;
+    private $_whereType;
+
+
+    protected setWhere($op, $col, $val)
     {
+        $this->_where[] = [$op, $col, $val];
     }
 
-    public function iexact()
+    public function exact($col, $val)
     {
+        $this->setWhere('exact', $col, $val);
     }
 
-    public function contains()
+    public function iexact($col, $val)
     {
+        $this->setWhere('iexact', $col, $val);
     }
 
-    public function icontains()
+    public function contains($col, $val)
     {
+        $this->setWhere('contains', $col, $val);
     }
 
-    public function regex()
+    public function icontains($col, $val)
     {
+        $this->setWhere('icontains', $col, $val);
     }
 
-    public function iregex()
+    public function regex($col, $val)
     {
+        $this->setWhere('regex', $col, $val);
     }
 
-    public function gt()
+    public function iregex($col, $val)
     {
+        $this->setWhere('iregex', $col, $val);
     }
 
-    public function gte()
+    public function gt($col, $val)
     {
+        $this->setWhere('gt', $col, $val);
     }
 
-    public function lt()
+    public function gte($col, $val)
     {
+        $this->setWhere('gte', $col, $val);
     }
 
-    public function lte()
+    public function lt($col, $val)
     {
+        $this->setWhere('lt', $col, $val);
     }
 
-    public function startswith()
+    public function lte($col, $val)
     {
+        $this->setWhere('lte', $col, $val);
     }
 
-    public function istartswith()
+    public function startswith($col, $val)
     {
+        $this->setWhere('startswith', $col, $val);
     }
 
-    public function endswith()
+    public function istartswith($col, $val)
     {
+        $this->setWhere('istartswith', $col, $val);
     }
 
-    public function iendswith()
+    public function endswith($col, $val)
     {
+        $this->setWhere('endswith', $col, $val);
     }
 
-    public function where($op = 'and', $data = null)
+    public function iendswith($col, $val)
     {
-        $data = $this->initData($data);
-        return $data;
+        $this->setWhere('iendswith', $col, $val);
     }
 
-    public function setMultiWhere($op = 'and', $data = null)
+    public function filter($op = 'and')
     {
-        $data = $this->initData($data);
-        return $data;
+        $this->_whereType = $op;
+        return $this;
+    }
+
+    public function setMultiFilter($opList)
+    {
+        return $this;
     }
 }
